a98c379b5d8a468b45cb6d65a300cccee781238d
ClicShopping - e-commerce Solution <39338579+ClicShopping@users.noreply.github.com>
2018-07-12 09:25:44 -0400

    add forgotten files

---
+++ sources/template/Default/modules/modules_products_info/content/products_info_url.php
@@ -0,0 +1,8 @@
+<div class="<?php echo $text_position; ?> col-md-<?php echo $content_width; ?>">
+  <div class="separator"></div>
+  <div itemprop="url" style="float: <?php echo MODULE_PRODUCTS_INFO_URL_POSITION; ?>;">
+    <div class="modulesProductsInfoUrl">
+      <span class= "modulesProductsInfoUrl"><h3><?php echo $products_url; ?></h3></span>
+    </div>
+  </div>
+</div>
