1626a6d661024a7e510fca19039c9b16e3e4dc95
ClicShopping <Yae8mr2o*>
2020-11-02 14:46:31 +0100

    little update

--- sources/template/Default/modules/modules_products_info/content/products_info_url.php
+++ sources/template/Default/modules/modules_products_info/content/products_info_url.php
@@ -1,6 +1,6 @@
 <div class="<?php echo $text_position; ?> col-md-<?php echo $content_width; ?>">
   <div class="separator"></div>
-  <div itemprop="url" style="float: <?php echo MODULE_PRODUCTS_INFO_URL_POSITION; ?>;">
+  <div style="float: <?php echo MODULE_PRODUCTS_INFO_URL_POSITION; ?>;">
     <div class="modulesProductsInfoUrl">
       <span class="modulesProductsInfoUrl"><h3><?php echo $products_manufactuer_url; ?></h3></span>
     </div>
